fba1487ced4dc5b8e595d2e1ac752722f5fe06d3
Brandon M. Crenshaw <109445618+brandononchain@users.noreply.github.com>
2025-09-15 17:28:24 -0500

    Update AppShell.tsx

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,1 +1,16 @@
+import { ReactNode } from "react";
+import { SideNav } from "./SideNav";
+import { Topbar } from "./Topbar";
+
+export default function AppShell({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-dvh">
+      <SideNav />
+      <div className="flex-1 flex flex-col">
+        <Topbar />
+        <main className="p-6 md:p-8">{children}</main>
+      </div>
+    </div>
+  );
+}
 
