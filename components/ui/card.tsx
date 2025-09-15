5e00a1eb7348f9b072bb85800a724c35df670607
Brandon M. Crenshaw <109445618+brandononchain@users.noreply.github.com>
2025-09-15 17:30:46 -0500

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,1 +1,16 @@
 
+import * as React from "react";
+import clsx from "clsx";
+
+export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={clsx("bg-elev border border-border rounded-lg shadow-card", className)} {...props} />;
+}
+export function CardHeader({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={clsx("p-4 border-b border-border", className)} {...props} />;
+}
+export function CardTitle({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
+  return <h3 className={clsx("font-semibold", className)} {...props} />;
+}
+export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={clsx("p-4", className)} {...props} />;
+}
