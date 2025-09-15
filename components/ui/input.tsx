1e42952276d98b3ac28b40a47cd4907419a7c053
Brandon M. Crenshaw <109445618+brandononchain@users.noreply.github.com>
2025-09-15 17:30:59 -0500

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,16 @@
 
+import * as React from "react";
+import clsx from "clsx";
+
+export function Input({ className, ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      className={clsx(
+        "flex h-9 w-full rounded-md border border-border bg-[#16161A] px-3 text-sm outline-none",
+        "focus:shadow-focus",
+        className
+      )}
+      {...props}
+    />
+  );
+}
